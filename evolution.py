863b3f0529079b0cae17a4baea31405ce1d5fb7f
BenoitGoutorbe <benoit.goutorbe@gmail.com>
2017-12-08 17:46:24 +0100

    Lire le fichier environment.dat et mesurer la fitness

--- evolution.py
+++ evolution.py
@@ -1,12 +1,20 @@
 import simulation as sim
+import os
+import numpy as np
 
 INI_file = "params.ini" #sys.argv[1]
 output_dir = "output" #sys.argv[2]
 
 
+env_file = open(os.path.join('tousgenesidentiques','environment.dat'))
+target_profile = []
+for line in env_file.readlines():
+    target_profile.append(float(line.split()[1]))
+target_profile= np.array(target_profile)
 
-def fitness (profil):
+def fitness (profile):
     #difference entre profil et profil cible
+    return np.mean(np.multiply(profile/np.sum(profile) -target_profile))
 
 def mutations () :
     #generer les mutations et réécrire les fichiers de tousgenesidentiques
@@ -14,6 +22,6 @@ def mutations () :
 
 def metropolis() :
     mutations()
-    sim.start_transcribing(INI_file, output_dir)
+    #sim.start_transcribing(INI_file, output_dir)
     profil_cible = []  # lire envirnoment.dat
     return []
