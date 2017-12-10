2e672bd14e494a57bd9de633fd912e8b1a63c61b
BenoitGoutorbe <benoit.goutorbe@gmail.com>
2017-12-10 16:42:45 +0100

    Lire la poition des genes dans les ficheirs
    j'ai aussi commenté des grosses parties du code originale qui servait pas, pour qu'on n'aie moins de mal a se repérer dans le code

--- evolution.py
+++ evolution.py
@@ -11,9 +11,32 @@ def fitness (profile, target_profile):
     # fitness = 1/distance
     return 1/np.sum(np.multiply(dif,dif))
 
-def mutations () :
-    #generer les mutations et réécrire les fichiers de tousgenesidentiques
+def read_positions () :
+    #lire les fichiers de tousgenesidentiques
+    config = sim.read_config_file(INI_file)
+    TSS_file = config.get('INPUTS', 'TSS')
+    TTS_file = config.get('INPUTS', 'TTS')
+    tss = sim.load_gff(TSS_file)
+    tts = sim.load_tab_file(TTS_file)
+    pos_start = tss['TSS_pos'].values
+    pos_end = tts['TTS_pos'].values
+    orientation  = sim.str2num(tss['TUorient'].values)
+    return [[pos_start[i],pos_end[i], orientation[i]] for i in range(len(pos_start))]
+
+def write_positions () :
+    #écrire les fichiers de tousgenesidentiques (TSS, TTS et GFF)
     return []
+
+def mutations (genome) :
+    positions = list(genome)
+    #inversion
+
+    #deletion
+
+    #insertion
+
+    write_positions(positions)
+    return positions
 
 def metropolis() :
     envir_file = open(os.path.join('tousgenesidentiques', 'environment.dat'))
@@ -22,10 +45,19 @@ def metropolis() :
         target_profile.append(float(line.split()[1]))
     target_profile = np.array(target_profile)
     envir_file.close()
-    mutations()
     #simulation :
+    current_genome = read_positions()
     transcriptome = np.array(sim.start_transcribing(INI_file, output_dir))
     current_fitness = fitness(transcriptome, target_profile)
+    hist_fitness = [current_fitness]
+
+    for i in range(2) :
+        current_genome = mutations(current_genome)
+        transcriptome = np.array(sim.start_transcribing(INI_file,output_dir))
+        current_fitness = fitness(transcriptome,target_profile)
+        print(current_fitness, transcriptome)
+        hist_fitness.append(current_fitness)
+
     print(current_fitness)
     return []
 
