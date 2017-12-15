d3d07e99d58b0495a9b9caade70a3d4f1f5a8d4b
BenoitGoutorbe <benoit.goutorbe@gmail.com>
2017-12-15 10:44:39 +0100

    Methodes inversion; insertion et deletion aboutie

--- evolution.py
+++ evolution.py
@@ -10,9 +10,11 @@ config = sim.read_config_file(INI_file)
 TSS_file = config.get('INPUTS','TSS')
 TTS_file = config.get('INPUTS','TTS')
 GFF_file = config.get('INPUTS', 'GFF')
-P_DEL = 0.01
-P_INS = 0.01
-P_INV = 0.01
+P_DEL = 0.0
+P_INS = 0.0
+P_INV = 1.0
+SIZE_INDEL = 1000
+
 
 def fitness (profile, target_profile):
     #difference entre profil et profil cible
@@ -44,16 +46,39 @@ def mutations (genome) :
         else :
             codant = codant + list(range(g[0],g[1]-1, -1))
     non_codant= [i for i in range(1,size_genome+1) if i not in codant]
-    print(len(codant), len(non_codant), non_codant[0],non_codant[-1])
 
     #inversion
-    if random.random() < P_INS :
-        
+    if random.random() < P_INV :
+        barrieres = random.sample(non_codant,2)
+        barrieres.sort()
+        print('INVERSION : intervalle', barrieres)
+        for g in positions :
+            if g[0] > barrieres[0] and g[1] < barrieres[1]:
+                g[0] = barrieres[1] - (g[0] - barrieres[0])
+                g[1] = barrieres[1] - (g[1] - barrieres[0])
+                g[2] = -1.0 * g[2]
 
     #deletion
+    if random.random() < P_DEL:
+        position_del = random.choice(non_codant)
+        while position_del+SIZE_INDEL in codant :
+            position_del = random.choice(non_codant)
+        print('DELETION', position_del)
+        size_genome = size_genome - SIZE_INDEL
+        for g in positions:
+            if g[0] > position_del and g[1] > position_del:
+                g[0] = g[0] - SIZE_INDEL
+                g[1] = g[1] - SIZE_INDEL
 
     #insertion
-
+    if random.random() < P_INS:
+        position_ins = random.choice(non_codant)
+        print('INSERTION', position_ins)
+        size_genome = size_genome + SIZE_INDEL
+        for g in positions:
+            if g[0] > position_ins and g[1] > position_ins:
+                g[0] = g[0] + SIZE_INDEL
+                g[1] = g[1] + SIZE_INDEL
     write_positions(positions)
     return positions
 
@@ -69,15 +94,15 @@ def metropolis() :
     transcriptome = np.array(sim.start_transcribing(INI_file, output_dir))
     current_fitness = fitness(transcriptome, target_profile)
     hist_fitness = [current_fitness]
-
+    print(current_fitness,transcriptome)
+    print(current_genome)
     for i in range(2) :
         current_genome = mutations(current_genome)
         transcriptome = np.array(sim.start_transcribing(INI_file,output_dir))
         current_fitness = fitness(transcriptome,target_profile)
         print(current_fitness, transcriptome)
+        print(current_genome)
         hist_fitness.append(current_fitness)
-
-    print(current_fitness)
     return []
 
 metropolis()
