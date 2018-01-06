9691f765658a7ec6b0bb07bb2e3276facef7eba9
Camille <camille.rouet@insa-lyon.fr>
2018-01-06 11:40:41 +0100

    Ajout de commentaires et de prints

--- evolution.py
+++ evolution.py
@@ -7,8 +7,6 @@ INI_file = "params.ini" #sys.argv[1]
 output_dir = "output" #sys.argv[2]
 trial_dir = "tousgenesidentiques" #sys.argv[2]
 
-# commentaire inutile pour un test
-
 config = sim.read_config_file(INI_file)
 TSS_file = config.get('INPUTS','TSS')
 TTS_file = config.get('INPUTS','TTS')
@@ -25,9 +23,10 @@ P_INS = 0.0
 P_INV = 1.0
 SIZE_INDEL = 1000
 
+# reinitialiser tousgenesidentiques
 def init_tousgenesidentiques():
-    # reinitialiser tousgenesidentiques
-        #lire les fichiers de tousgenesidentiques_init
+     #lire les fichiers de tousgenesidentiques_init
+    print('REINITIALISATION DU GENOME')
     tss = sim.load_gff(TSS_file_init)
     tts = sim.load_tab_file(TTS_file_init)
     pos_start = tss['TSS_pos'].values
@@ -43,15 +42,16 @@ def init_tousgenesidentiques():
     write_positions(positions_genes, positions_barrieres, size_genome)
     return []
 
-
+# retourne la fitness du profil en le comparant au profil cible
 def fitness (profile, target_profile):
     #difference entre profil et profil cible
     dif = profile/np.sum(profile) - target_profile
     # fitness = 1/distance
     return 1/np.sum(np.multiply(dif,dif))
 
+
+#lire les fichiers de tousgenesidentiques
 def read_positions () :
-    #lire les fichiers de tousgenesidentiques
     tss = sim.load_gff(TSS_file)
     tts = sim.load_tab_file(TTS_file)
     pos_start = tss['TSS_pos'].values
@@ -64,14 +64,15 @@ def read_positions () :
     positions_barrieres = prot['prot_pos'].values
     return (positions_genes, positions_barrieres, size_genome)
 
+#écrire tous les fichiers du dossier tousgenesidentiques (TSS, TTS et GFF)
 def write_positions (pos_genes, pos_bar, size_genome) :
-    #écrire les fichiers de tousgenesidentiques (TSS, TTS et GFF)
     write_TTS(pos_genes)
     write_TSS(pos_genes)
     write_tousgenesidentiques(pos_genes, size_genome)
     write_prot(pos_bar)
     return []
 
+# ecrire les genes dans TTS
 def write_TTS(positions_genes) :
     file = open(os.path.join(trial_dir,'TTS.dat'), 'w+')
     file.write("TUindex\tTUorient\tTTS_pos\tTTS_proba_off\n")
@@ -84,7 +85,8 @@ def write_TTS(positions_genes) :
         file.write(str(v[1]) + "\t" + "1.\n")
     file.close()
     return []
-        
+
+# ecrire les genes dans TSS
 def write_TSS(positions_genes) :
     file = open(os.path.join(trial_dir,'TSS.dat'), 'w+')
     file.write("TUindex\tTUorient\tTSS_pos\tTSS_strength\n")
@@ -97,7 +99,8 @@ def write_TSS(positions_genes) :
         file.write(str(v[0]) + "\t" +".2\n")
     file.close()
     return []
-        
+
+# ecrire les genes dans tousgenesidentiques.gff
 def write_tousgenesidentiques(positions_genes, size_genome) :
     file = open(os.path.join(trial_dir,'tousgenesidentiques.gff'), 'w+')
     file.write("##gff-version 3\n#!gff-spec-version 1.20\n#!processor NCBI annotwriter\n##sequence-region tousgenesidentiques 1 ")
@@ -112,7 +115,7 @@ def write_tousgenesidentiques(positions_genes, size_genome) :
     file.close()
     return []
 
-    
+# ecrire les genes dans prot
 def write_prot(posbar) :
     file = open(os.path.join(trial_dir,'prot.dat'), 'w+')
     file.write("prot_name\tprot_pos\n")
@@ -121,23 +124,28 @@ def write_prot(posbar) :
     file.close()
     return []
 
-
+# effectue une mutation sur le genome
+# inversion avec une probabilite P_INV
+# deletion avec une probabilite P_DEL
+# insertion avec une probabilite P_INS
 def mutations (pos_genes, pos_barrieres, size_genome) :
     positions_genes= list(pos_genes)
     positions_barrieres = list(pos_barrieres)
+    # detection des sites codants et non codants
     codant = []
     for g in positions_genes :
-        if g[2] > 0 :
+        if g[2] > 0 : # si le gene est dans le sens positif
             codant = codant + list(range(g[0],g[1]+1))
-        else :
+        else : # si le gene est dans le sens negatif
             codant = codant + list(range(g[0],g[1]-1, -1))
     non_codant = [i for i in range(1,size_genome+1) if i not in codant]
 
     #inversion
     if random.random() < P_INV :
+        # choix de deux bornes dans l'ordre croissant dans le non codant
         bornes = random.sample(non_codant,2)
-        bornes.sort()
-        print('INVERSION : intervalle', bornes)
+        bornes.sort() 
+        print('INVERSION SUR L\'INTERVALLE', bornes)
         for g in positions_genes :
             if g[0] > bornes[0] and g[1] < bornes[1]:
                 g[0] = bornes[1] - (g[0] - bornes[0])
@@ -177,6 +185,7 @@ def mutations (pos_genes, pos_barrieres, size_genome) :
     return positions_genes, positions_barrieres, size_genome
 
 
+# algorithme Metropolis sur le genome
 def metropolis() :
     envir_file = open(os.path.join('tousgenesidentiques', 'environment.dat'))
     target_profile = []
@@ -189,8 +198,8 @@ def metropolis() :
     transcriptome = np.array(sim.start_transcribing(INI_file, output_dir))
     current_fitness = fitness(transcriptome, target_profile)
     hist_fitness = [current_fitness]
-    print(current_fitness,transcriptome)
-    print(current_genome)
+    print('FITNESS :', current_fitness, '\nEXPRESSION :', transcriptome)
+    print('GENOME :\n',current_genome)
     for i in range(2) :
         current_genome, current_barrieres, current_size = mutations(current_genome,current_barrieres, current_size)
         transcriptome = np.array(sim.start_transcribing(INI_file,output_dir))
@@ -199,6 +208,6 @@ def metropolis() :
         print(current_genome)
         hist_fitness.append(current_fitness)
     return []
-    
+
 init_tousgenesidentiques()
 metropolis()
